5a6fdbf93fb4687d9200cc9dc50d64004c6768ba
Hamim Alam <82621331+alamhamim@users.noreply.github.com>
2023-03-19 00:30:19 -0400

    Hey it works!!

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@
 curl -sL <https://raw.githubusercontent.com/alamhamim/vangchur_git/main/vangchur_git.sh> | bash -s
 
 # Print a message to let the user know the 'vangchur' alias has been created
-echo "The 'vangchur' alias has been created. ||| Please do not use this in prod environment :)"
+echo "The 'git_vangchur' alias has been created. ||| Please do not use this in prod environment :)"
--- vangchur_git.sh
+++ vangchur_git.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
-#Funny msg for git commit -m 
-commit_msg = $(fortune)
 
-# Define the 'vangchur' alias with the generated commit message
-git config --global alias.vangchur "!git add . && git commit -m \"$commit_msg\" && git push"
+# Generate a random commit message using fortune
+commit_msg="Hey it works!!"
+
+# Check the user's shell and update the appropriate configuration file
+if [[ "$SHELL" == "/bin/bash" ]]; then
+  echo "alias git_vangchur='git add . && git commit -m \"$commit_msg\" && git push'" >> ~/.bashrc
+  source ~/.bashrc
+elif [[ "$SHELL" == "/bin/zsh" ]]; then
+  echo "alias git_vangchur='git add . && git commit -m \"$commit_msg\" && git push'" >> ~/.zshrc
+  source ~/.zshrc
+elif [[ "$SHELL" == "/bin/sh" ]] || [[ "$SHELL" == "/usr/bin/sh" ]]; then
+  echo "alias git_vangchur='git add . && git commit -m \"$commit_msg\" && git push'" >> ~/.bash_profile
+  source ~/.bash_profile
+else
+  echo "Your Machine Sucks!!! Get a new one!!!"
+fi
 
 # Print a message to let the user know the alias has been created
-echo "The 'vangchur' alias has been created with the following commit message:"
+echo "The 'git_vangchur' alias has been created with the following commit message:"
 echo "$commit_msg"
